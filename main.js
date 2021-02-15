c7ff541fc9cd249de8d72ea476ea7896fc011dc5
alvaripi <50096369+alvaripi@users.noreply.github.com>
2021-02-15 13:53:21 +0200

    commit

---
+++ main.js
@@ -0,0 +1,33 @@
+function avaaSelaus(){
+    document.querySelector(".selaus").classList.toggle("nayta");
+    document.querySelector(".navbar").classList.toggle("nayta");
+}
+
+function avaaKukka1(){
+    document.querySelector(".kukat_1_tausta").classList.toggle("nayta");
+    document.querySelector(".kukat_1_info").classList.toggle("nayta");
+    
+}
+
+function avaaKukka2(){
+    document.querySelector(".kukat_2_tausta").classList.toggle("nayta");
+    document.querySelector(".kukat_2_info").classList.toggle("nayta");
+    
+}
+
+function avaaKukka3(){
+    document.querySelector(".kukat_3_tausta").classList.toggle("nayta");
+    document.querySelector(".kukat_3_info").classList.toggle("nayta");
+    
+}
+
+function avaaKukka4(){
+    document.querySelector(".kukat_4_tausta").classList.toggle("nayta");
+    document.querySelector(".kukat_4_info").classList.toggle("nayta");
+    
+}
+
+
+
+
+
